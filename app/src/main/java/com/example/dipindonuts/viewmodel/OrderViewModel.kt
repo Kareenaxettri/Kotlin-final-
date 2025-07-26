793b8844b8af13af293806e7178daa0f24338e3e
kareena <kareenatandan477@gmail.com>
2025-07-26 14:57:23 +0545

    Update cart

--- app/src/main/java/com/example/dipindonuts/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/example/dipindonuts/viewmodel/OrderViewModel.kt
@@ -25,7 +25,7 @@ class OrderViewModel @Inject constructor(
     private val _sellerOrdersState = MutableStateFlow<OrdersState>(OrdersState.Loading)
     val sellerOrdersState: StateFlow<OrdersState> = _sellerOrdersState.asStateFlow()
     
-    private val _orderCreationState = MutableStateFlow<OrderCreationState>(OrderCreationState.Idle)
+    //private val _orderCreationState = MutableStateFlow<OrderCreationState>(OrderCreationState.Idle)
     val orderCreationState: StateFlow<OrderCreationState> = _orderCreationState.asStateFlow()
     
     fun loadUserOrders(userId: String) {
